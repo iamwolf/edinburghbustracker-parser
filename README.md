7b4f56fa743ca37bdd04485774ecabef20548f88
Wolf Merkt <w.merkt@gmail.com>
2014-02-06 14:10:54 +0000

    Added official API parser route, deleted scraper since it breaks down frequently due to changes in the website structure, removed mock-API route, added test API route' '

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 #edinburghbustracker API
-####[Demo](edinburghbustracker.herokuapp.com/potterow)
-Quick'n'dirty app to parse data from the mobile website of mybustracker.co.uk until I receive access to the official API.
+####[Demo](bustracker-api.rentawolf.com/potterow)
+~~Quick'n'dirty app to parse data from the mobile website of mybustracker.co.uk until I receive access to the official API.~~
+Parser deactivated since classes and structure changes frequently. Now operating using official API key and preparing data for mobile app. Raw API key has to be set using environment variables.
 
-Currently used to return information on the Edinburgh University Shuttle Contract with Lothian Buses (Service C134).
+Currently used to return information on the Edinburgh University Shuttle Contract with Lothian Buses (Service C134) as well as Service 41 operating on a similar route and using the same bus stops.
 
 ###Supported routes
-* /mock-official-api - Returns sample output for testing purposes
+~~* /mock-official-api - Returns sample output for testing purposes~~
 * /potterow - Returns Edinburgh Uni Shuttle departures from Potterow to Kings Buildings
 * /meadows - Returns Edinburgh Uni Shuttle departures from Giffords Park to Kings Buildings
 * /kb - Returns Edinburgh Uni Shuttle departures from the Roger Land Building at Kings Buildings to Central
--- app.js
+++ app.js
@@ -3,6 +3,8 @@
  */
 var express = require('express');
 var http = require('http');
+var request = require('request');
+var crypto = require('crypto');
 var jsdom = require('jsdom');
 
 var app = express();
@@ -23,10 +25,10 @@ if ('development' == app.get('env')) {
 /**
   * Configuration variables: bus stop codes & URLs
   */
-/*var busStop_Potterow = '36253583';
+var busStop_Potterow = '36253583';
 var busStop_GiffordsPark = '36238273';
 var busStop_KingsBuildings = '36245896';
-var urlKingsToCentral = 'http://www.mybustracker.co.uk/?module=mobile&busStopDest=36253583&busStopCode=36245896';
+/*var urlKingsToCentral = 'http://www.mybustracker.co.uk/?module=mobile&busStopDest=36253583&busStopCode=36245896';
 var urlPotterowToKings = 'http://www.mybustracker.co.uk/?module=mobile&busStopDest=36245896&busStopCode=36253583';
 var urlGiffordToKings = 'http://www.mybustracker.co.uk/?module=mobile&busStopDest=36245896&busStopCode=36238273';*/
 
@@ -35,10 +37,51 @@ var urlGiffordToKings = 'http://www.mybustracker.co.uk/?module=mobile&busStopDes
   */
 app.get('/', function(req, res) { 
     res.writeHead(200,{'content-type':'text/json'});
-    res.end(JSON.stringify("More information on GitHub - please use correct routes")); 
+    res.end(JSON.stringify("More information on GitHub [https://github.com/iamwolf/edinburghbustracker-parser] - please use correct routes")); 
+});
+
+app.get('/test', function(req, res) {
+	buses = [{number: 'Uni-Shuttle', arrivalTime: 'DUE'}, {number: '41', arrivalTime: '5'}, {number: 'Uni-Shuttle', arrivalTime: '30'}, {number: '41', arrivalTime: '35'}];
+	res.send(JSON.stringify(buses));
 });
 
 app.get('/:from', function(req, res) {
+    var busstop = (req.param('from')=='kb') ? busStop_KingsBuildings : (req.param('from')=='meadows') ? busStop_GiffordsPark : busStop_Potterow;
+
+    var d = new Date();
+    var dateString = d.getFullYear().toString() + ('0' + (d.getMonth()+1)).slice(-2) + ('0' + d.getDate()).slice(-2) + ('0' + d.getHours()).slice(-2);
+    var requestAPIKey = crypto.createHash('md5').update(process.env.APIKEY + dateString).digest('hex');
+    var apiURI = 'http://ws.mybustracker.co.uk/?module=json&key=' + requestAPIKey + '&function=getBusTimes&stopId=' + busstop;
+
+    request(apiURI, function (error, response, t) {
+        if (!error && response.statusCode == 200) {
+            var buses = [];
+            t = JSON.parse(t);
+
+            for (i in t.busTimes) {
+                if(['55','126',''].join(',').indexOf(t.busTimes[i].refService+',')>-1) {
+                    for (j in t.busTimes[i].timeDatas) {
+                        var bus = {};
+                        bus.number = t.busTimes[i].mnemoService;
+                        bus.number = (bus.number=='C134') ? 'Uni-Shuttle' : bus.number;
+                        bus.arrivalTime=t.busTimes[i].timeDatas[j].minutes;
+                        buses.push(bus);
+                    }
+                }
+
+                if (i==(t.busTimes.length-1)) {
+                    buses.sort(function(a,b) {
+                        return (a.arrivalTime>b.arrivalTime);
+                    });
+
+                    res.send(JSON.stringify(buses));
+                }
+            }
+        }
+    });
+});
+
+/*app.get('/parsed-data/:from', function(req, res) {
     // NB: switch-construct didn't work, let's do it quick'n'dirty with IFs
     if (['kb','meadows','potterow','randomlongstringtoaddanotheropportunity'].join(',').indexOf(req.param('from')+',')>-1) {
         kb = 'http://www.mybustracker.co.uk/?module=mobile&busStopDest=36253583&busStopCode=36245896';
@@ -51,18 +94,18 @@ app.get('/:from', function(req, res) {
           function (errors, window) {
             var buses = [];
 
-            if (window.$('table.timesTable tbody tr:visible td.time a').length>0) {
-                for (var i=0; i<window.$('table.timesTable tbody tr:visible td.time a').length; i++) {
-                    var busNumber = window.$(window.$('table.timesTable tbody tr:visible:not(.tHeader):not(.tFooter):not(.tResult)')[i]).children().first().text();
+            if (window.$('table.timesTable tbody tr:visible:not(.tHeader):not(.tResult):not(.tFooter) td.time a').length>0) {
+                for (var i=0; i<window.$('table.timesTable tbody tr:visible:not(.tHeader):not(.tResult):not(.tFooter) td.time a').length; i++) {
+                    var busNumber = window.$(window.$('table.timesTable tbody tr:visible:not(.tHeader):not(.tResult):not(.tFooter)')[i]).children().first().text();
                     busNumber = (busNumber=='C134') ? 'Uni-Shuttle' : busNumber;
-                    var busDestination = window.$(window.$('tr:visible td.dest')[i]).text();
-                    var busArrivalTime = window.$(window.$('tr:visible td.time a')[i]).text();
+                    var busDestination = window.$(window.$('tr:visible:not(.tHeader):not(.tResult):not(.tFooter) td.dest')[i]).text();
+                    var busArrivalTime = window.$(window.$('tr:visible:not(.tHeader):not(.tResult):not(.tFooter) td.time a')[i]).text();
                     console.log("Bus No. ", busNumber, "to", busDestination, "departs in", busArrivalTime);
 
                     var bus = {number: busNumber, arrivalTime: busArrivalTime};
                     buses.push(bus);
 
-                    if (i==(window.$('table.timesTable tbody tr:visible td.time a').length-1)) {
+                    if (i==(window.$('table.timesTable tbody tr:visible:not(.tHeader):not(.tResult):not(.tFooter) td.time a').length-1)) {
                         buses.sort(function(a,b) {
                             return (parseInt(a.arrivalTime)>parseInt(b.arrivalTime));
                         });
@@ -72,42 +115,13 @@ app.get('/:from', function(req, res) {
             } else {
                 res.end(JSON.stringify(buses));
             }
+          	window.close(); // save/free memory
           }
         );
     } else {
         res.end('Sorry, we do not support this bus stop');
     }    
-});
-
-/**
-  * Route used in development to test code against the official JSON-format
-  */
-app.get('/mock-official-api', function(req, res) {
-    var buses = {
-            "stopCode": "36232658",
-            "stopName": "Haymarket Satio",
-            "services": [
-                    { "serviceName": "3", "route": "Mayfield -- Clovenstone", "buses": [
-                                    { "destination": "CLOVENSTONE", "arrivalTime": "5", "accessible": true },
-                                    { "destination": "CLOVENSTONE", "arrivalTime": "*30", "accessible": false }
-                            ]
-                    },
-                    { "serviceName": "25", "route": "Riccarton -- Restalrig", "buses": [
-                                    { "destination": "RICCARTON", "arrivalTime": "*12", "accessible": false },
-                                    { "destination": "RICCARTON", "arrivalTime": "42", "accessible": true }
-                            ]
-                    },
-                    { "serviceName": "33", "route": "Westburn -- Ferniehill", "buses": [
-                                    { "destination": "BABERTON", "arrivalTime": "22", "accessible": true },
-                                    { "destination": "LONGSTONE", "arrivalTime": "45", "accessible": true }
-                            ]
-                    }
-            ]
-    };
-
-    res.writeHead(200, {'content-type':'text/json'});
-    res.end(JSON.stringify(buses));
-});
+});*/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
